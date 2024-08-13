02b261dd41816f2795459d6c6d47221de8015c63
cloeywatson81 <cwatson@student.neumont.edu>
2024-08-12 23:27:17 -0600

    hiii

---
+++ TextGame/src/main/java/com/example/textgame/controller/MainApp.java
@@ -0,0 +1,37 @@
+/**
+ * @author cwatson
+ * @createdOn 8/12/2024 at 11:08 PM
+ * @projectName TextGame
+ * @packageName com.example.textgame.controller;
+ */
+package com.example.textgame.controller;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+
+import java.io.IOException;
+
+public class MainApp extends Application {
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("view/Gui.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+        stage.setTitle("Text Game");
+        stage.setScene(scene);
+        stage.setOnCloseRequest((WindowEvent event) -> {
+            Platform.exit();
+            System.exit(0);
+        });
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+}
