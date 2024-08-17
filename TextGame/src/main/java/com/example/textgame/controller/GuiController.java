91b7c852d625718ef3b8cc220e5244cf6695e439
Khayden Roberts <122504123+Hoochymoochy@users.noreply.github.com>
2024-08-16 18:54:00 -0600

    Room's messing (#3)

--- TextGame/src/main/java/com/example/textgame/controller/GuiController.java
+++ TextGame/src/main/java/com/example/textgame/controller/GuiController.java
@@ -1,8 +1,20 @@
 package com.example.textgame.controller;
 
+import com.example.textgame.LoadData;
+import com.example.textgame.LoadRoom;
 import com.example.textgame.controller.Design.Sound;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.util.Duration;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 
 /**
  * @author cwatson
@@ -12,10 +24,48 @@ import javafx.scene.control.Button;
  */
 
 public class GuiController {
+    @FXML
+    Label Entry1, Entry2, Entry3, description, roomName;
+    ArrayList<HashMap> currentRoom =  new ArrayList<>();
+    HashMap hashMap = new HashMap();
+    @FXML
+    private void initialize() {
+        LoadData data = new LoadData();
+        hashMap = data.Data(new File("check.txt"));
+        setUp(new LoadRoom().room());
+        HideText();
+        showDescription();
+    }
+
+    @FXML
+    private void move(){
+        setUp(new LoadRoom().House());
+    }
+    public void setUp(HashMap create){
+        roomName.setText((String) create.get("Name"));
+        description.setText((String) create.get("Description"));
+        //can also put image and items in here too
+    }
 
 
-@FXML protected void buttonClick(){
-    System.out.println("I was clicked!");
-    new Sound().clickButton(); //Testing
-}
+    public void buttonClick(){
+
+    }
+    public void HideText(){
+        Entry1.setVisible(false);
+        Entry2.setVisible(false);
+        Entry3.setVisible(false);
+    }
+    public void ShowText(){
+        Entry1.setVisible(true);
+        Entry2.setVisible(true);
+        Entry3.setVisible(true);
+    }
+    public void HideDescription(){
+        description.setVisible(false);
+    }
+    public void showDescription(){
+        description.setVisible(true);
+    }
+
 }
